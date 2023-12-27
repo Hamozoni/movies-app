05aa97737e54cd8a9ef40e2da0a936f6c26cf7c9
Mohamed Yahia <m.yahya199290@gmail.com>
2023-12-27 03:43:10 -0800

    finshing review card

--- src/Components/ReviewCard/ReviewCard.jsx
+++ src/Components/ReviewCard/ReviewCard.jsx
@@ -1,9 +1,28 @@
 
 import "./ReviewCard.scss";
 
-const ReviewCard = ()=> {
+const ReviewCard = ({review})=> {
   return (
     <div className="review-card">
+        <header className="review-header">
+             <div className="img">
+                <img src={process.env.REACT_APP_BASE_URL + "original" + review?.author_details?.avatar_path} alt={review?.author} />
+             </div>
+             <div className="rev-titles">
+                 <h4 className="name"></h4>
+                 <div className="date-rate">
+                     <div className="rate"></div>
+                      <p className="wrettin"></p>
+                      <span className="date">
+                      </span>
+                 </div>
+             </div>
+        </header>
+        <div className="review-content">
+            <aside className="review">
+
+            </aside>
+        </div>
 
     </div>
   )
