21d8717c70804424f03c4c92ea11ca3bee5b9d91
Mohamed Yahia <m.yahya199290@gmail.com>
2023-12-28 06:39:44 -0800

    creating movie stitisics component

--- src/Components/MovieMedia/MovieMedia.jsx
+++ src/Components/MovieMedia/MovieMedia.jsx
@@ -2,13 +2,15 @@ import { useContext, useEffect, useState } from "react";
 import { globalContext } from "../../GlobalStateContext/GlobalContext";
 import fetchData from "../../Utilities/fetchData";
 
+import "./MovieMedia.scss";
+
 
 const MovieMedia = ({id})=> {
 
     const [changes,sethanges] = useState({});
     const [mediaVed,setMediaVed] = useState([]);
-    const [video,setVideo] = useState({});
-    const [selection,setSelecion] = useState('video');
+    const [videos,setVideos] = useState({});
+    const [selection,setSelecion] = useState('posters');
 
     const {lang} = useContext(globalContext);
 
@@ -16,43 +18,64 @@ const MovieMedia = ({id})=> {
         fetchData(`movie/${id}/changes?language=${lang}&page=1`)
         .then((data)=>{
             sethanges(data?.changes);
-            setVideo(data?.changes.find( video => video.key === 'videos'))
+            setVideos(data?.changes.find( video => video.key === 'videos'))
             console.log(data?.changes)
         })
 
         fetchData(`movie/${id}/images`)
         .then((data)=>{
             setMediaVed(data);
+            console.log(data)
+
         })
-    },[id,lang,selection]);
+    },[id,lang]);
 
     return (
         <section className='movie-media'>
+            <hr/>
             <header className="movie-media-header">
-                <h4 className="title"></h4>
                 <nav className="media-nav">
-                    <h5>videos</h5>
-                    <h5>backdrops</h5>
-                    <h5>posters</h5>
+                    <h4 className="title">
+                        media
+                    </h4>
+                    <div className="links">
+                        <button 
+                            onClick={()=> setSelecion('backdrops')}
+                            >
+                                backdrops {mediaVed?.backdrops?.length}
+                        </button>
+                        <button 
+                            onClick={()=> setSelecion('posters')}
+                           >
+                                posters {mediaVed?.posters?.length}
+                        </button>
+                    </div>
                 </nav>
                 <button className="view-all">
-
-                </button>
+                     veiw all backdrops
+                </button> 
             </header>
             <div className="media-content">
-                <div className="img-container">
-                    {/* {  
-                      selection === 'vedio' ?
-                      changes?.map((media)=>(
-                        media?.key === "videos" &&
-                        <img src="" alt="" />
-                      )) :
+                    {  
+                      selection === 'backdrops' ?
+                      mediaVed?.backdrops?.map((media)=>(
+                        <div className="img-container" key={media?.file_path}>
+                                <img src={process.env.REACT_APP_BASE_URL + 'original' + media?.file_path} alt="" />
+                        </div>
+                      )) : selection === 'posters' ?
+
+                      mediaVed?.posters?.map((media)=>(
+                        <div className="img-container poster" key={media?.file_path}>
+                                <img src={process.env.REACT_APP_BASE_URL + 'original' + media?.file_path} alt="" />
+                        </div>
+                      )) 
+                      :
 
                       ''
-                    } */}
+                    }
 
-                </div>
             </div>
+            <hr />
         </section>
     )
 }
