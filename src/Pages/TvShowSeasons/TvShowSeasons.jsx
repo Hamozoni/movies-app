058c3cea7e50fa74042dac6b717dbd2406b31820
Mohamed Yahia <m.yahya199290@gmail.com>
2024-01-04 08:31:59 -0800

    creating tv shows seasons page

---
+++ src/Pages/TvShowSeasons/TvShowSeasons.jsx
@@ -0,0 +1,44 @@
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
+import { globalContext } from "../../GlobalStateContext/GlobalContext";
+import fetchData from "../../Utilities/fetchData";
+
+
+const TvShowSeasons = () => {
+
+    const {id} = useParams();
+    const {lang} = useContext(globalContext);
+
+    const [seasons,setSeasons] = useState();
+
+
+    useEffect(()=>{
+
+        fetchData(`tv/${id}?language=${lang}`)
+        .then((data)=>{
+            setSeasons(data)
+        })
+
+    },[id,lang]);
+
+
+  return (
+    <main className="tv-seasons">
+        <div className="tv-s-container">
+            <header className="seasons-header">
+                <div className="tv-details">
+                    <div className="tv-image">
+
+                    </div>
+                    <div className="tv-details">
+                        
+                    </div>
+                </div>
+            </header>
+
+        </div>
+    </main>
+  )
+}
+
+export default TvShowSeasons
