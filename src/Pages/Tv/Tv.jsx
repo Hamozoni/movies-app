5ae130aee6021771198088c463aae4e94fe2f7fb
Mohamed Yahia <m.yahya199290@gmail.com>
2024-01-05 08:26:29 -0800

    adding style to tv shows page

--- src/Pages/Tv/Tv.jsx
+++ src/Pages/Tv/Tv.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import fetchData from '../../Utilities/fetchData';
 import { globalContext } from '../../GlobalStateContext/GlobalContext';
 import MovieTvCover from '../../Components/MovieTvCover/MovieTvCover';
@@ -8,6 +8,8 @@ import MovieSocial from '../../Components/MovieSocial/MovieSocial';
 import MovieMedia from '../../Components/MovieMedia/MovieMedia';
 import Recommendations from '../../Components/Recommendations/Recommendations';
 import TvSeasonCard from '../../Components/TvSeasonCard/TvSeasonCard';
+
+import "./Tv.scss";
 
 const Tv = () => {
     const {id} = useParams();
@@ -31,10 +33,18 @@ const Tv = () => {
             <section className='tv-content'>
                  <section className='cast'>
                     <TopBilledCast type='tv' id={id} title='Series Cast'/>
-                    {
-                        details?.seasons?.length &&
-                       <TvSeasonCard  tvShow={details?.seasons[details?.seasons?.length - 1]}/>
-                    }
+                    <section className='tv-season'>
+                        <h4 className="sea-name">
+                               Current Season
+                        </h4>
+                            {
+                                details?.seasons?.length &&
+                            <TvSeasonCard  tvShow={details?.seasons[details?.seasons?.length - 1]}/>
+                            }
+                        <Link className='to-seasons' to={`/tv/${id}/seasons`}>
+                            view all seasons
+                        </Link>
+                    </section>
                     <MovieSocial id={id} section='reviews' mediaType='tv'/>
                     <MovieMedia id={id} mediaType='tv' />
                     <Recommendations id={id} mediaType='tv'/>
