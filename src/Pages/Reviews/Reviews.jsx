3e14ce586c3d04224fbb83c4ec038d9b8535e067
Mohamed Yahia <m.yahya199290@gmail.com>
2024-01-23 20:04:18 +0300

    adding review card to reviews page

--- src/Pages/Reviews/Reviews.jsx
+++ src/Pages/Reviews/Reviews.jsx
@@ -1,8 +1,30 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
+import fetchData from "../../Utilities/fetchData";
+import ReviewCard from "../../Components/ReviewCard/ReviewCard";
 
 
 const Reviews = () => {
+
+    const {id} = useParams();
+    const [reviews,setReviews] = useState([]);
+
+    useEffect(()=>{
+        fetchData(`movie/${id}/reviews?language=en-US&page=1`)
+        .then(data => {
+            setReviews(data?.results);
+        })
+    },[id]);
+
+
   return (
-    <div>Reviews</div>
+    <section>
+        {
+            reviews?.map((review)=> (
+                <ReviewCard key={review?.id}  review={review}/>
+            ))
+        }
+    </section>
   )
 }
 
