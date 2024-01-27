83f7c1ba228c6cb1cc24afd5cd20ef0e8b8c2c88
Mohamed Yahia <m.yahya199290@gmail.com>
2024-01-27 17:12:03 +0300

    styling cast

--- src/Pages/Cast/Cast.jsx
+++ src/Pages/Cast/Cast.jsx
@@ -2,17 +2,21 @@ import React, { useEffect, useState } from 'react'
 import MainMediaNav from '../../Components/MainMediaNav/MainMediaNav'
 import fetchData from '../../Utilities/fetchData';
 import { useParams } from 'react-router-dom';
+import PersonCard from '../../Components/PersonCard/PersonCard';
+
+import "./Cast.scss";
 
 const Cast = () => {
 
     const [cast,setCast] = useState({});
+    const [crew,setCrew] = useState();
     const {id} = useParams()
 
     useEffect(()=>{
        fetchData(`movie/${id}/credits?language=en-US`)
        .then((data)=>{
           setCast(data);
-          console.log(data)
+          setCrew(Object.groupBy(data?.crew, ({ department}) => department))
        })
     },[id]);
 
@@ -23,9 +27,33 @@ const Cast = () => {
         </header>
         <section>
             <h5>cast {cast?.cast?.length}</h5>
+            <div className="cast-content">
+                {
+                    cast?.cast?.map((person)=>(
+                        <PersonCard key={person?.id} person={person}/>
+                    ))
+                }
+            </div>
         </section>
         <section>
-            <h5>crew {cast?.crew?.length}</h5>
+            <h5 className='cr-t'>crew {cast?.crew?.length}</h5> 
+            <div className="cast-content">
+                {
+                    
+                    Object.entries(crew)?.map((p)=>(
+                        <>
+                        <h5>{p[0]}</h5>
+                        {
+                            p[1]?.map((person)=>(
+
+                                <PersonCard key={person?.id} person={person}/>
+                            ))
+                        }
+                        </>
+                    ))
+                    
+                }
+            </div>
         </section>
     </div>
   )
