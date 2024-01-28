96b4a5e65fc3fbb2a69a2181bdc5c9907d4dab4a
Mohamed Yahia <m.yahya199290@gmail.com>
2024-01-28 21:25:23 +0300

    making titles tabel

--- src/Pages/AlternitaveTitles/AlternativeTitles.jsx
+++ src/Pages/AlternitaveTitles/AlternativeTitles.jsx
@@ -12,19 +12,18 @@ const AlternativeTitles = () => {
 
         fetchData(`movie/${id}/alternative_titles`)
         .then(title=>{
-            setTiltes(title?.titles);
-            console.log(title?.titles);
+            setTiltes(Object.groupBy(title.titles,ti => {
+                return ti.iso_3166_1
+            }));
+           
             fetchData(`configuration/countries?language=en-US`)
             .then(data=> {
                     const newCoutListArray = []
                     data?.map((el)=>{
 
                         if(title?.titles?.some(e=> e.iso_3166_1 === el.iso_3166_1)) {
-                            //   setCountries(prev=> [prev,el])
                             newCoutListArray.push(el);
                             console.log(newCoutListArray);
-
-
                         }
                     });
                     setCountries(newCoutListArray)
@@ -35,15 +34,15 @@ const AlternativeTitles = () => {
         })
     },[id]);
 
-
+    console.log(Object.entries(titles));
   return (
     <main className="alt-titles">
         <header className="alt-header">
             <MainMediaNav />
         </header>
         <div className="alt-content">
-            <section className='alt-menu'>
-                <header className='menu-header'>
+            <section className='alt-cout-list'>
+                <header className='cout-header'>
                      <h3>Alternative Titles</h3>
                      <p>{titles?.length}</p>
                 </header>
@@ -54,7 +53,7 @@ const AlternativeTitles = () => {
                                 
                             <li>
                                 <span>{cout?.native_name}</span>
-                                <span></span>
+                                <span>1</span>
                             </li>
 
                             
@@ -62,6 +61,41 @@ const AlternativeTitles = () => {
                     }
                            
                 </ul>
+            </section>
+            <section className='alt-t'>
+                {
+                    Object.entries(titles)?.map((title)=>(
+                        <tabel key={title?.title} className="titles-card" style={{display: 'block'}}>
+                           <thead>
+                              <tr>
+                                 
+                                    {
+                                        countries?.map((cout)=>(
+                                            cout?.iso_3166_1 === title[0] && 
+                                            <h3>{cout?.native_name}</h3>
+                                        ))
+                                    }
+                                
+                              </tr>
+                           </thead>
+                           <tbody>
+                               <tr>
+                                  <td>title</td>
+                                  <td>type</td>
+                               </tr>
+                               {
+                                  title[1]?.map((t)=>(
+                                    <tr key={t?.title}>
+                                        <td>{t?.title}</td>
+                                        <td >{t?.type}</td>
+                                    </tr>
+
+                                  ))
+                               }
+                           </tbody>
+                        </tabel>
+                    ))
+                }
             </section>
         </div>
     </main>
