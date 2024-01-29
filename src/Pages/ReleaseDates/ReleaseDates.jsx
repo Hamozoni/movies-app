c0785bf48e340dab841ae44d841f20217f240875
Mohamed Yahia <m.yahya199290@gmail.com>
2024-01-29 19:43:42 +0300

    fetching data in release dates

--- src/Pages/ReleaseDates/ReleaseDates.jsx
+++ src/Pages/ReleaseDates/ReleaseDates.jsx
@@ -7,13 +7,26 @@ const ReleaseDates = () => {
 
     const {id} = useParams();
     const [dates,setDates] = useState([]);
+    const [countries,setCountries] = useState([]);
 
     useEffect(()=>{
 
         fetchData(`movie/${id}/release_dates`)
         .then((dates)=>{
-            setDates(dates.results);
-            console.log(dates.results)
+            setDates(Object.groupBy(dates.results,date=> date.iso_3166_1));
+            console.log(Object.groupBy(dates.results,date=> date.iso_3166_1));
+            fetchData(`configuration/countries?language=en-US`)
+            .then((count)=>{
+                const countriesList = Object.groupBy(count,c=> c.iso_3166_1);
+                const filteredCountriesList = [];
+                
+                dates.results?.map((date)=>{
+                    filteredCountriesList.push(countriesList[date.iso_3166_1][0])
+                })
+
+                setCountries(filteredCountriesList);
+                console.log(filteredCountriesList);
+            })
         })
 
     },[id]);
