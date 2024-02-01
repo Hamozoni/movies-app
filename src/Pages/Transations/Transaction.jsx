a5051d1fe81c0ea9045a7a0004a9f34b6beb18a7
Mohamed Yahia <m.yahya199290@gmail.com>
2024-02-02 01:36:21 +0300

    creating transation component

---
+++ src/Pages/Transations/Transaction.jsx
@@ -0,0 +1,21 @@
+import MainMediaNav from "../../Components/MainMediaNav/MainMediaNav"
+
+const Transaction = () => {
+  return (
+    <main className="mov-transation">
+        <header className="tran-header">
+            <MainMediaNav />
+        </header>
+        <div className="trans-box">
+            <section className="trans-left-part">
+
+            </section>
+            <section> 
+
+            </section>
+        </div>
+    </main>
+  )
+}
+
+export default Transaction
