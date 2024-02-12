72234a907094b0c5e33076521b441dd317372957
Mohamed Yahia <m.yahya199290@gmail.com>
2024-02-12 20:16:02 +0300

    creating free to watch section in home pag

--- src/Components/FreeToWatch/FreeToWatch.jsx
+++ src/Components/FreeToWatch/FreeToWatch.jsx
@@ -1,7 +1,39 @@
 const FreeToWatch = ()=> {
     return (
         <div className="free-to-watch">
-
+            <section className={`${type} free-to-watch trending`}>
+                <div className="trending-container">
+                    <header className="trend-header">
+                        <h3 className="trend-title">
+                            {languages[lang].trending + " "}  
+                            {type === 'movie' ? languages[lang].movies : type === 'tv'  ? languages[lang].tvShows :languages[lang].people }
+                        </h3>
+                        <nav className="trend-nav">
+                            <ul>
+                                <li 
+                                    className={filter === 'day' && 'active'}
+                                    onClick={()=> setFilter('day')}
+                                    >
+                                    {languages[lang].today}
+                            </li>
+                                <li 
+                                    className={filter === 'week' && 'active'}
+                                    onClick={()=> setFilter('week')}
+                                    >
+                                    {languages[lang].thisWeek}
+                                </li>
+                            </ul>
+                        </nav>
+                    </header>
+                    <div className="movies">
+                        {
+                            movies?.map((movie)=>(
+                                <MovieCard movie={movie} type={type} />
+                            ))
+                        }
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
