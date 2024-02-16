a8b6405d32bfb4de78240a2e14b3a6f436ff260a
Mohamed Yahia <m.yahya199290@gmail.com>
2024-02-16 21:54:52 +0300

    fetching popular people data

--- src/Pages/PopularPersons/PopularPresons.jsx
+++ src/Pages/PopularPersons/PopularPresons.jsx
@@ -1,8 +1,33 @@
+import { useEffect, useState } from "react"
+import fetchData from "../../Utilities/fetchData";
+import PersonCard from "../../Components/PersonCard/PersonCard";
 
 
 const PopularPresons = () => {
+
+    const [page,setPage] = useState(1);
+    const [persons,setPersons] = useState([]);
+
+    useEffect(()=>{
+        fetchData(`person/popular?language=en-US&page=${page}`)
+        .then((data)=> {
+            setPersons(data?.results)
+            console.log(data?.results)
+        })
+    },[page]);
+
   return (
-    <div>PopularPresons</div>
+    <section className="popular-people">
+        <h4>popular people</h4>
+        <div className="persons-cards">
+
+            {
+                persons?.map((person)=> (
+                    <PersonCard key={person?.id} person={person}/>
+                ))
+            }
+        </div>
+    </section>
   )
 }
 
