0a10a33158aad73bcb389f0720ddb08c28585429
Mohamed Yahia <m.yahya199290@gmail.com>
2024-03-01 22:07:15 +0300

    creating trailer player

---
+++ src/Components/TrailerPlayer/TrailerPlayer.jsx
@@ -0,0 +1,16 @@
+
+const TrailerPlayer = ({mediaId}) => {
+
+  return (
+    <section className='trailer-player'>
+        <div className="trialer-container">
+            <header className="tr-header">
+                <h4 className="off-tr">Official Trailer</h4>
+            </header>
+        </div>
+
+    </section>
+  )
+}
+
+export default TrailerPlayer
