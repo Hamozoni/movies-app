49dfc8b15eb3297ad87b2aebf86ae6a4ad1c614a
Mohamed Yahia <m.yahya199290@gmail.com>
2024-03-03 14:53:46 +0300

    styling trailer player

--- src/Components/TrailerPlayer/TrailerPlayer.jsx
+++ src/Components/TrailerPlayer/TrailerPlayer.jsx
@@ -4,6 +4,8 @@ import ReactPlayer from 'react-player';
 import fetchData from '../../Utilities/fetchData';
 import { globalContext } from '../../GlobalStateContext/GlobalContext';
 
+import "./TrailerPlayer.scss"
+
 const TrailerPlayer = () => {
 
     const {mediaType,mediaId,setIsTrailer} = useContext(globalContext);
@@ -23,20 +25,17 @@ const TrailerPlayer = () => {
 
   return (
     <section className='trailer-player'>
-        {
-            videoId && 
         <div className="trialer-container">
             <header className="tr-header">
                 <h4 className="off-tr">Official Trailer</h4>
                 <span className="cancel" onClick={()=> setIsTrailer(false)}>
                     <CloseRoundedIcon />
                 </span>
+            </header>
                 <div className="tr-player">
                     <ReactPlayer playing={true} controls url={`https://www.youtube.com/watch?v=${videoId}`} />
                 </div>
-            </header>
         </div>
-}
 
     </section>
   )
