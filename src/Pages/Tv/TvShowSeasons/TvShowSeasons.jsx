70ca02c1959cc4ab81c1668a916eb80a7e0aac82
Mohamed Yahia <m.yahya199290@gmail.com>
2024-03-25 02:51:03 +0300

    modified:   src/Components/Header/Header.scss         deleted:    src/Components/homeComponents/freeToWatch/FreeToWatch.scss         modified:   src/Components/homeComponents/homeSearchBar/SearchBar.scss         modified:   src/Components/homeComponents/latestTrailer/LatestTrailer.scss         modified:   src/Components/homeComponents/trending/Trending.scss         modified:   src/GlobalStyle/_variables.scss         deleted:    src/Pages/Home/Home.scss         modified:   src/Pages/Tv/TvShowSeasons/TvShowSeasons.jsx

--- src/Pages/Tv/TvShowSeasons/TvShowSeasons.jsx
+++ src/Pages/Tv/TvShowSeasons/TvShowSeasons.jsx
@@ -1,41 +1,21 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext} from "react";
 import {  useParams } from "react-router-dom"
-
-
-
-
 import "./TvShowSeasons.scss";
-import { globalContext } from "../../../GlobalStateContext/GlobalContext";
-import fetchData from "../../../utilities/fetchData";
 import TvSeasonCard from "../../../Components/tvComponents/tvSeasonCard/TvSeasonCard";
+import { tvShowDetailsContext } from "../../../Layouts/tvShowsLayout/TvShowsLayout";
 
 
 const TvShowSeasons = () => {
 
     const {id} = useParams();
-    const {lang} = useContext(globalContext);
-
-    const [seasons,setSeasons] = useState();
-
-
-    useEffect(()=>{
-       
-
-        fetchData(`tv/${id}?language=${lang}`)
-        .then((data)=>{
-            setSeasons(data);
-            console.log(data)
-        })
-
-    },[id,lang]);
-
+    const {details} = useContext(tvShowDetailsContext);
 
   return (
     <main className="tv-seasons">
         <div className="tv-s-container">
             <div className="tv-seasons-content">
                 {
-                 seasons?.seasons?.map((tvShow)=>(
+                 details?.seasons?.map((tvShow)=>(
                     <TvSeasonCard key={tvShow?.id} tvShow={tvShow} id={id}/>
                  ))
                 }
