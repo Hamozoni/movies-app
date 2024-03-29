410ff51e624aea8c7cc75bf7d1e61f3a65d25625
Mohamed Yahia <m.yahya199290@gmail.com>
2024-03-29 15:41:06 +0300

    modified:   src/Components/error/Error.jsx         modified:   src/Components/error/error.scss

--- src/Components/error/Error.jsx
+++ src/Components/error/Error.jsx
@@ -1,15 +1,22 @@
+import { useContext } from 'react';
+import {globalContext} from "../../GlobalStateContext/GlobalContext";
 import './error.scss';
 
 const Error = ({error,height,onClick}) => {
+
+  const {lang,theme} = useContext(globalContext);
   return (
     <div className="error" style={{height}}>
         <div className="error-container" >
-            <h3 className="er-m">{error?.message}</h3>
+            <h3 className={`t-color-${theme}-3 er-m`}>
+               {error?.message}
+            </h3>
             <button 
                   onClick={onClick}
-                  className='try-btn' 
+                  className={`try-btn t-color-${theme}`} 
                   type="button"
-                  > try agin
+                  > 
+                    {lang === 'en' ? 'try agin' : 'حاول مرة اخرى'}
             </button>
         </div>
     </div>
